f850cbf608fb0dcc61f7146ef830d47c3a744fa1
Mikko Dequina <dequina1547@gmail.com>
2019-12-16 17:07:15 +0800

    storing scroll position in a proper way

--- lib/widgets/listbuilder.dart
+++ lib/widgets/listbuilder.dart
@@ -8,11 +8,7 @@ class DoubleHolder {
 class StatefulListView extends StatefulWidget {
   final DoubleHolder offset = new DoubleHolder();
 
-  StatefulListView(this._itemCount,
-      this._indexedWidgetBuilder,
-      {
-        Key key
-      })
+  StatefulListView(this._itemCount, this._indexedWidgetBuilder, {Key key})
       : super(key: key);
 
   double getOffsetMethod() {
@@ -32,39 +28,32 @@ class StatefulListView extends StatefulWidget {
 }
 
 class _StatefulListViewState extends State<StatefulListView> {
-
   ScrollController scrollController;
   final int _itemCount;
   final IndexedWidgetBuilder _itemBuilder;
-  double scrollpos = 0.0;
+ 
 
   _StatefulListViewState(this._itemCount, this._itemBuilder);
-
+  
   @override
   void initState() {
-    super.initState();
-    SharedPref().getScrollPos().then((value){
-         setState(() {
-            scrollpos = value;
-         });
-         print(scrollpos);
+    SharedPref().getScrollPos().then((value) {
+      scrollController = new ScrollController(initialScrollOffset: value);
     });
-    scrollController = new ScrollController(
-        initialScrollOffset: scrollpos
-    );
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return new NotificationListener(
       child: new ListView.builder(
-          controller: scrollController, 
-          itemCount: _itemCount,
-          itemBuilder: _itemBuilder
+        controller: scrollController,
+        itemCount: _itemCount,
+        itemBuilder: _itemBuilder,
+        physics: ClampingScrollPhysics(),
       ),
       onNotification: (notification) {
         if (notification is ScrollNotification) {
-          // widget.setOffsetMethod(notification.metrics.pixels);
           SharedPref().setScrollPos(notification.metrics.pixels);
         }
       },
